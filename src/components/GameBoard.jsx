d0d35b5a54eea56887aeba323fc0116fa1075c54
0xkiichiro <barisaytimur@gmail.com>
2022-08-18 20:05:50 +0300

    updt

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -15,13 +15,28 @@ const GameBoard = () => {
     { id: 8, content: "" },
   ]);
 
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 6, 8],
+    [0, 4, 8],
+  ];
+
   const handleClick = (e) => {
     console.log(boardArr);
+    // players turn
     setBoardArr(
       boardArr.map((item) =>
-        item.id == e.target.id ? { ...item, content: "X" } : item
+        item.id == e.target.id && item.content == ""
+          ? { ...item, content: "X" }
+          : item
       )
     );
+    //computers turn
+
     console.log(e.target.id);
   };
 
